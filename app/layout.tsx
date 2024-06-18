443d15e9a597dbe7539423aecdd0c536ac5758ec
unknown <ariaworkshopnft@gmail.com>
2024-06-18 12:04:05 +0300

    repair shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,24 @@
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { Providers } from "./providers";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
