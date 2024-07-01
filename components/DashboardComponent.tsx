a39a04ca147e6e67e1e2683eaac3f1544837337f
unknown <ariaworkshopnft@gmail.com>
2024-07-01 16:44:27 +0300

    add dollar sign

--- components/DashboardComponent.tsx
+++ components/DashboardComponent.tsx
@@ -72,7 +72,7 @@ export default function DashBoardComponent(data: any ) {
                   </CardHeader>
                   <CardContent>
                     {/* //REQUEST EXPECTED AMOUNT TOTAL */}
-                    <div className="text-2xl font-bold">{data.data.totalExpectedAmount}</div>
+                    <div className="text-2xl font-bold">${data.data.totalExpectedAmount}</div>
                     <p className="text-xs text-muted-foreground">
                       +20.1% from last month
                     </p>
